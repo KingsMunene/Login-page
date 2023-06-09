0a2775de605739b8cc13767e4bbbd50e3d9b37f9
KingsMunene <munenekings2@gmail.com>
2023-06-09 23:03:45 +0300

    Login form column composables

--- app/src/main/java/com/example/loginpage/uiData/LoginScreen.kt
+++ app/src/main/java/com/example/loginpage/uiData/LoginScreen.kt
@@ -1,34 +1,77 @@
 package com.example.loginpage.uiData
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loginpage.R
 
 @Composable
-fun LoginPage() {
+fun LoginPage(modifier: Modifier = Modifier){
 
-    LoginForm()
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(R.string.page_title),
+        style = MaterialTheme.typography.titleLarge)
+
+        LoginForm()
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+                Text(stringResource(R.string.login_button))
+            }
+
+            Button(onClick = { }, modifier = Modifier.fillMaxWidth()) {
+                Text(stringResource(R.string.signup_button))
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginForm(modifier: Modifier = Modifier) {
-    Card(){
-        Column(){
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)){
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
             OutlinedTextField(value = "" ,
                 onValueChange = {},
                 label = { Text(text = stringResource(id = R.string.user_name)) },
                 singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Next
                 ),
@@ -42,6 +85,7 @@ fun LoginForm(modifier: Modifier = Modifier) {
                 onValueChange = {},
                 isError = false,
                 singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
                 label = { Text(stringResource(R.string.Password_label)) },
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Done
@@ -55,8 +99,8 @@ fun LoginForm(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PagePreview() {
-    LoginPage()
+    LoginPage(modifier = Modifier)
 }
