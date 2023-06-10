f3be211996ffa09293dffc8e3523d73ae2bcff46
KingsMunene <munenekings2@gmail.com>
2023-06-10 23:16:24 +0300

    verticalScroll modifier on the column

--- app/src/main/java/com/example/loginpage/uiData/LoginScreen.kt
+++ app/src/main/java/com/example/loginpage/uiData/LoginScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 
@@ -45,7 +47,8 @@ fun LoginPage(loginViewModel: LoginViewModel = viewModel(),
 
     Column(modifier = modifier
         .fillMaxSize()
-        .padding(mediumPadding),
+        .padding(mediumPadding)
+        .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
